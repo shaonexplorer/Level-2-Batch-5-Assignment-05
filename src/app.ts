6b72cfa70d137f9cc4dd23dc60de299fba874f1d
shaonexplorer <shaonexplorer@gmail.com>
2025-08-22 19:54:54 +0600

    cors policy updated

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import { parcelRouter } from "./app/modules/parcel/parcel.routes";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: `http://localhost:5173`, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
