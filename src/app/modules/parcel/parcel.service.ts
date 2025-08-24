ae61dd5b6210b684a1df5b276c95a0ef863e227d
shaonexplorer <shaonexplorer@gmail.com>
2025-08-24 12:57:00 +0600

    create parcel service updated

--- src/app/modules/parcel/parcel.service.ts
+++ src/app/modules/parcel/parcel.service.ts
@@ -108,9 +108,9 @@ const getMe = async (req: Request) => {
     throw new AppError(404, "Sender not found");
   }
 
-  const parcels = await Parcel.find({ sender: sender._id }).populate(
-    "receiver"
-  );
+  const parcels = await Parcel.find({ sender: sender._id })
+    .populate("receiver")
+    .sort("-createdAt");
 
   return parcels;
 };
